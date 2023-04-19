a0b4432121159552819782503775aee96744314e
sumin <csumin008255@gmail.com>
2023-04-19 11:43:56 +0900

    feat: Bye기능 추가 - ByeClass, ByeClassTest 추가

---
+++ course_student/java-project2/src/main/java/com/example/ByeClass.java
@@ -0,0 +1,7 @@
+package com.example;
+
+public class ByeClass {
+    public void print() {
+        System.out.println("Bye");
+    }
+}
---
+++ course_student/java-project2/src/main/java/com/example/ByeClassTest.java
@@ -0,0 +1,8 @@
+package com.example;
+
+public class ByeClassTest {
+    public static void main(String[] args){
+        ByeClass byeClass = new ByeClass();
+        byeClass.print();
+    }
+}
