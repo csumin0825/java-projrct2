edd5f65cff591d0953a2a9970cd61ec2a3e2d8bc
sumin <csumin008255@gmail.com>
2023-04-20 13:38:29 +0900

    fix: AverageEx 수정 - 0.3 -> 3.0

--- course_student/java-project2/src/main/java/com/example/javaproject2/week1/day4/AverageEx.java
+++ course_student/java-project2/src/main/java/com/example/javaproject2/week1/day4/AverageEx.java
@@ -6,6 +6,6 @@ public class AverageEx {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.printf("%.1f",((sc.nextInt() + sc.nextInt() +sc.nextInt())/0.3));
+        System.out.printf("%.1f",((sc.nextInt() + sc.nextInt() +sc.nextInt())/3.0));
     }
 }
