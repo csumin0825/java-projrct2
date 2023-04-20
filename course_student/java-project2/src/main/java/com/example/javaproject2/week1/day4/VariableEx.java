5fd90d68be95e31ec119198814252b0f26b2e519
sumin <csumin008255@gmail.com>
2023-04-20 15:12:37 +0900

    feat: VariableEx 추가

---
+++ course_student/java-project2/src/main/java/com/example/javaproject2/week1/day4/VariableEx.java
@@ -0,0 +1,16 @@
+package com.example.javaproject2.week1.day4;
+
+import com.example.PrintHello;
+
+public class VariableEx {
+    public static void main(String[] args) {
+        int iVal;
+        iVal = 0; // 최초로 값을 지정하는 것을 초기화
+        PrintHello printHello; // 변수 선언
+        printHello = new PrintHello();
+
+        printHello.print();
+        System.out.println(iVal);
+
+    }
+}
