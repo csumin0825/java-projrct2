ffda982953f08e8c42270d55c08e754c9afb3f8b
sumin <csumin008255@gmail.com>
2023-04-20 11:22:27 +0900

    feat: ScannerEx, PrintfEx, PrintWhy 추가

---
+++ course_student/java-project2/src/main/java/com/example/javaproject2/week1/day4/PrintfEx.java
@@ -0,0 +1,12 @@
+package com.example.javaproject2.week1.day4;
+
+public class PrintfEx {
+    public void print(){
+        System.out.printf("%s","Hello");
+    }
+
+    public static void main(String[] args) {
+        PrintfEx printfEx = new PrintfEx(); //포켓볼에서 포켓몬 꺼내기
+        printfEx.print(); //물대포 공격
+    }
+}
--- course_student/java-project2/src/main/java/com/example/javaproject2/week1/day4/ScannerEx.java
+++ course_student/java-project2/src/main/java/com/example/javaproject2/week1/day4/ScannerEx.java
@@ -7,7 +7,7 @@ public class ScannerEx {
         Scanner sc = new Scanner(System.in);
         System.out.println("두개의 숫자를 입력하세요. 입력하고 엔터 입력하고 엔터");
         System.out.print("첫 번째 숫자: ");
-        System.out.println("첫 번째 숫자는 "+sc.nextInt()+"입니다.");
+        System.out.println("첫가 번째 숫자는 "+sc.nextInt()+"입니다.");
         System.out.print("두 번째 숫자: ");
         System.out.println("두 번째 숫자는 "+sc.nextInt()+"입니다.");
     }
