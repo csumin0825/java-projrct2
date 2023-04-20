a0061fe98839bda2f9b603212f81c7e6dfc6a820
sumin <csumin008255@gmail.com>
2023-04-20 10:42:21 +0900

    feat: ScannerEx, ScannerExTest 추가

---
+++ course_student/java-project2/src/main/java/com/example/javaproject2/week1/day4/ScannerExTest.java
@@ -0,0 +1,10 @@
+package com.example.javaproject2.week1.day4;
+
+import java.util.Scanner;
+
+public class ScannerExTest {
+    public static void main(String[] args) {
+        ScannerEx scannerEx = new ScannerEx();
+        scannerEx.inputTwoNumbersAndPlus();
+    }
+}
