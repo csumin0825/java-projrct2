93b85bc11bfa29af11672bc2ca4d513d7da9481a
sumin <csumin008255@gmail.com>
2023-04-20 15:27:06 +0900

    feat: DefineAndIntialize, ScannerAndVariable 추가

---
+++ course_student/java-project2/src/main/java/com/example/javaproject2/week1/day4/DefineAndIntialize.java
@@ -0,0 +1,16 @@
+package com.example.javaproject2.week1.day4;
+
+import com.example.PrintHello;
+
+import java.util.Scanner;
+
+public class DefineAndIntialize {
+    public static void main(String[] args) {
+        int iVal = 1;
+        PrintHello printHello = new PrintHello();
+        Scanner scanner = new Scanner(System.in);
+
+        Scanner sc;
+        sc = new Scanner(System.in);
+    }
+}
