d522f64f2cae24a0b2068e2c602e54e876c8bec4
sumin <csumin008255@gmail.com>
2023-04-20 14:35:14 +0900

    feat: ByteTypeExercise, ScannerFormatting 추가

---
+++ course_student/java-project2/src/main/java/com/example/javaproject2/week1/day4/ByteTypeExercise.java
@@ -0,0 +1,12 @@
+package com.example.javaproject2.week1.day4;
+
+public class ByteTypeExercise {
+    public static void main(String[] args) {
+        byte byte1 = 10;
+        byte byte2 = -128;
+//        byte byte3 = -129; 오류
+//        byte byte4 = 128; 오류
+
+
+    }
+}
