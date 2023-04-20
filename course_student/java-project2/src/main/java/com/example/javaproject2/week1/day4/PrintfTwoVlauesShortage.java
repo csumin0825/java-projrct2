0f4cda5965dcac5ba7b90b29828d6beca22982a3
sumin <csumin008255@gmail.com>
2023-04-20 11:24:10 +0900

    feat: PrintfTwoVlauesShortage 추가

---
+++ course_student/java-project2/src/main/java/com/example/javaproject2/week1/day4/PrintfTwoVlauesShortage.java
@@ -0,0 +1,7 @@
+package com.example.javaproject2.week1.day4;
+
+public class PrintfTwoVlauesShortage {
+    public static void main(String[] args) {
+        System.out.printf("%s님 안녕하세요. 그런데 제가 하고싶은 말은 %s입니다.","최수민");
+    }
+}
