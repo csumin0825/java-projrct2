dc16b4da537fbce00df83ffd004b48adf62b3633
sumin <csumin008255@gmail.com>
2023-04-20 11:48:16 +0900

    feat: PrintOfDate 추가

---
+++ course_student/java-project2/src/main/java/com/example/javaproject2/week1/PrintOfDate.java
@@ -0,0 +1,12 @@
+package com.example.javaproject2.week1;
+
+public class PrintOfDate {
+    public static void main(String[] args) {
+        System.out.printf("%d\n",4);
+        System.out.printf("%02d\n",4);
+        System.out.printf("%02d\n",11);
+
+        //2023-04-20
+        System.out.printf("%d-%02d-%02d\n",2023,4,20);
+    }
+}
