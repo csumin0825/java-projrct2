e3506e35d9864fdff87d15e3e7bea68109ec8a55
sumin <csumin008255@gmail.com>
2023-04-20 09:48:41 +0900

    feat: ScannerLine, ScannerLineTest 추가

---
+++ course_student/java-project2/src/main/java/com/example/javaproject2/week1/day4/ScannerLineTest.java
@@ -0,0 +1,8 @@
+package com.example.javaproject2.week1.day4;
+
+public class ScannerLineTest {
+    public static void main(String[] args){
+        ScannerLine scannerLineTest = new ScannerLine();
+        scannerLineTest.mergeLine();
+    }
+}
