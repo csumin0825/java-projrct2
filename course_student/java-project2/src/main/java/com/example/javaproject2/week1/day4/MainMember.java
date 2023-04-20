992cd545a2ce8a7a39d193b2ced815f4eaf1094f
sumin <csumin008255@gmail.com>
2023-04-20 15:50:52 +0900

    feat: MainMember 추가

---
+++ course_student/java-project2/src/main/java/com/example/javaproject2/week1/day4/MainMember.java
@@ -0,0 +1,9 @@
+package com.example.javaproject2.week1.day4;
+
+public class MainMember {
+    int iVal;
+    public static void main(String[] args) {
+        MainMember mainMember = new MainMember();
+        System.out.println(mainMember.iVal);
+    }
+}
