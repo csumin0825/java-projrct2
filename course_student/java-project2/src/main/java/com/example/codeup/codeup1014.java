f2d1b2e31ebdebdd1c182797844893b36c513051
sumin <csumin008255@gmail.com>
2023-04-20 15:31:02 +0900

    feat: codeup1014 추가

---
+++ course_student/java-project2/src/main/java/com/example/codeup/codeup1014.java
@@ -0,0 +1,12 @@
+package com.example.codeup;
+
+import java.util.Scanner;
+
+public class codeup1014 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String val1 = sc.next();
+        String val2 = sc.next();
+        System.out.printf("%s %s", val2,val1);
+    }
+}
