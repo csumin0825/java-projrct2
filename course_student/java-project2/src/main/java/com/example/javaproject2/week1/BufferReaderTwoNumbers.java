fac1d63d5f7bdaa05bd91c5fc51bbacd3e00f8bd
sumin <csumin008255@gmail.com>
2023-04-20 09:16:10 +0900

    feat: BufferREaderTwoNumbers 추가

---
+++ course_student/java-project2/src/main/java/com/example/javaproject2/week1/BufferReaderTwoNumbers.java
@@ -0,0 +1,12 @@
+package com.example.javaproject2.week1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BufferReaderTwoNumbers {
+    public void plusTwoNums() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println(br.readLine()+br.readLine());
+    }
+}
