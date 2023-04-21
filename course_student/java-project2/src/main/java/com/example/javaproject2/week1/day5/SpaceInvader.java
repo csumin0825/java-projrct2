f95e1ae577feb1d9a592c0be3cb81f28e2a4b0a4
sumin <csumin008255@gmail.com>
2023-04-21 13:04:16 +0900

    fix: SpaceInvader 수정

--- course_student/java-project2/src/main/java/com/example/javaproject2/week1/day5/SpaceInvader.java
+++ course_student/java-project2/src/main/java/com/example/javaproject2/week1/day5/SpaceInvader.java
@@ -4,10 +4,19 @@ public class SpaceInvader {
     private void moveLeft() {
         int location = 0;
         location = -1;
+        System.out.printf("moveLeft : %d\n",location);
     }
 
     private void moveRight() {
         int location = 0;
         location = 1;
+        System.out.printf("moveRight : %d\n",location);
+    }
+
+
+    public static void main(String[] args) {
+        SpaceInvader si = new SpaceInvader();
+        si.moveLeft();
+        si.moveRight();
     }
 }
