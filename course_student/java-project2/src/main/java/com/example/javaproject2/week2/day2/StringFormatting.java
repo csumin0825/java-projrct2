c9ddec0f3b24fa2dfa37ca8438cb724902c300f9
sumin <csumin008255@gmail.com>
2023-04-25 13:46:22 +0900

    feat: StringEquals,StringFormatting, StringSplit 추가

---
+++ course_student/java-project2/src/main/java/com/example/javaproject2/week2/day2/StringFormatting.java
@@ -0,0 +1,11 @@
+package com.example.javaproject2.week2.day2;
+
+public class StringFormatting {
+    public static void main(String[] args) {
+        String name = "최수민";
+        int balance = 10000;
+
+        String result = name + "님의 통장 잔고는 "+balance+"원 입니다.";
+        System.out.println(result);
+    }
+}
