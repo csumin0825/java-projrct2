c039ff6ff46b8684586926c32dc62e73405d76be
sumin <csumin008255@gmail.com>
2023-04-25 15:13:09 +0900

    feat: RefrenceTypeArray 추가

---
+++ course_student/java-project2/src/main/java/com/example/javaproject2/week2/day2/RefrenceTypeArray.java
@@ -0,0 +1,13 @@
+package com.example.javaproject2.week2.day2;
+
+import java.util.Arrays;
+
+public class RefrenceTypeArray {
+    public static void main(String[] args) {
+        String[] sArr = new String[3];
+        System.out.println(Arrays.toString(sArr));
+
+        float[] fArr = new float[3];
+        System.out.println(Arrays.toString(fArr));
+    }
+}
