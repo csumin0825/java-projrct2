99b9a66a4ad39a716e71139670dbf168d1fcaa80
sumin <csumin008255@gmail.com>
2023-04-26 09:25:52 +0900

    feat: codeup1041 추가

---
+++ course_student/java-project2/src/main/java/com/example/codeup/codeup1041.java
@@ -0,0 +1,14 @@
+package com.example.codeup;
+
+import java.util.Scanner;
+
+public class codeup1041 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String val = sc.next();
+//        int ascii =
+    }
+}
+
+
