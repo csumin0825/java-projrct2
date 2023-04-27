e98b9f1b52746c1a9a9a38eff913a9d7dd5ec95d
sumin <csumin008255@gmail.com>
2023-04-27 10:15:19 +0900

    feat: Accumulate687Compound, IncreaseOperator 추가

---
+++ course_student/java-project2/src/main/java/com/example/javaproject2/week2/day4/IncreaseOperator.java
@@ -0,0 +1,11 @@
+package com.example.javaproject2.week2.day4;
+
+public class IncreaseOperator {
+    public static void main(String[] args) {
+        int i =10;
+//        i++;x
+        System.out.println(i++);
+        System.out.println(++i);
+
+    }
+}
