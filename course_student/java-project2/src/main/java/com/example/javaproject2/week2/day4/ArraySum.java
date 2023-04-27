e39204ba52e1b8de4339c8add428c46a71aa037c
sumin <csumin008255@gmail.com>
2023-04-27 09:36:19 +0900

    feat: ArraySum, CompoundAssignmentOperator 추가

---
+++ course_student/java-project2/src/main/java/com/example/javaproject2/week2/day4/ArraySum.java
@@ -0,0 +1,16 @@
+package com.example.javaproject2.week2.day4;
+
+public class ArraySum {
+    public static void main(String[] args) {
+        int answer = 0;
+        int[] arr = {2,1,7,4};
+        System.out.println(arr[0]);
+
+        answer = answer + arr[0];
+        answer = answer + arr[1];
+        answer = answer + arr[2];
+        answer = answer + arr[3];
+
+        System.out.printf("%d" ,answer);
+    }
+}
