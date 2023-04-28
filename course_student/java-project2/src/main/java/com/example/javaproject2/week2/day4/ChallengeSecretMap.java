5279b920f2ce11726627ff73840ef5af907245b6
sumin <csumin008255@gmail.com>
2023-04-28 09:08:53 +0900

    feat: ChellengeSecretMap 추가

---
+++ course_student/java-project2/src/main/java/com/example/javaproject2/week2/day4/ChallengeSecretMap.java
@@ -0,0 +1,15 @@
+package com.example.javaproject2.week2.day4;
+
+public class ChallengeSecretMap {
+    public static void main(String[] args) {
+        int n = 5;
+        int[] arr1 = {9, 20, 28, 18, 11};
+        int[] arr2 = {30, 1, 21, 17, 28};
+
+//        int n = 6;
+//        int[] arr1 = {46, 33, 33, 22, 31, 50};
+//        int[] arr2 = {27, 56, 19, 14, 14, 10};
+
+        System.out.println(arr1[0] | arr2[0]);
+    }
+}
