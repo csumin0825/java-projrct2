406be628682f3ba62a82b126a0604a90234e976f
sumin <csumin008255@gmail.com>
2023-05-01 10:39:41 +0900

    feat: codeup1103, 1106 추가

---
+++ course_student/java-project2/src/main/java/com/example/codeup/codeup1106.java
@@ -0,0 +1,8 @@
+package com.example.codeup;
+
+public class codeup1106 {
+    public static void main(String[] args) {
+        System.out.println(Integer.MIN_VALUE + " " + Integer.MAX_VALUE);
+
+    }
+}
