9d50d96e09569cde961d424a321430de8a190c3c
sumin <csumin008255@gmail.com>
2023-05-08 14:54:26 +0900

    feat: Greet, GreetTest 추가

---
+++ course_student/java-project2/src/main/java/com/example/javaproject2/week4/day1/GreetTest.java
@@ -0,0 +1,9 @@
+package com.example.javaproject2.week4.day1;
+
+public class GreetTest {
+    public static void main(String[] args) {
+        // alt + 1
+        Greet greet = new Greet(); //인스턴스 생성 , 인스턴스화 -> heap영역에 올림(메모리에?)
+        greet.printHello();
+    }
+}
