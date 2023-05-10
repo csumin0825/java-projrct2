1781dc20d83395d200f07b62f87490529af91495
sumin <csumin008255@gmail.com>
2023-05-10 13:07:52 +0900

    feat: DrawDiamond, DrawDiamond_noRepeat 추가

--- course_student/java-project2/src/main/java/com/example/javaproject2/week4/day3/DrawDiamond.java
+++ course_student/java-project2/src/main/java/com/example/javaproject2/week4/day3/DrawDiamond.java
@@ -5,23 +5,19 @@ public class DrawDiamond {
         return symbol.repeat(n);
     }
 
-    public static String makeALine( int h, int i){
-        int pivot = h/2;
-        if (i <= pivot) {
-            // 피라미드 출력
-            // 초항이 3 등차가 -2
-            return String.format("%s%s\n", getRepeatedSymbol(" ", pivot - i), getRepeatedSymbol("*", 2 * i + 1));
-        } else {
-            // 역피라미드 출력
-            return String.format("%s%s\n", getRepeatedSymbol(" ", i - pivot), getRepeatedSymbol("*", 2 * (h - i) - 1));
-        }
-    }
-
     public static void main(String[] args) {
         int h = 5;
         int pivot = h / 2;
         for (int i = 0; i < h; i++) {
-            System.out.printf(makeALine(h,i));
+//            System.out.printf("pivot: %d %d %d\n", pivot, i, -2 + h - i - 2);
+            if (i <= pivot) {
+                // 피라미드 출력
+                // 초항이 3 등차가 -2
+                System.out.printf("%s%s\n", getRepeatedSymbol(" ", pivot - i), getRepeatedSymbol("*", 2 * i + 1));
+            } else {
+                // 역피라미드 출력
+                System.out.printf("%s%s\n", getRepeatedSymbol(" ", i - pivot), getRepeatedSymbol("*", 2 * (h - i) - 1));
+            }
         }
     }
 }
--- course_student/java-project2/src/main/java/com/example/javaproject2/week4/day3/DrawDiamond_noRepeat.java
+++ course_student/java-project2/src/main/java/com/example/javaproject2/week4/day3/DrawDiamond_noRepeat.java
@@ -9,19 +9,24 @@ public class DrawDiamond_noRepeat {
         return answer;
     }
 
+    public static String makeALine(int h, int i) {
+        int pivot = h / 2;
+        if (i <= pivot) {
+            // 피라미드 출력
+            // 초항이 3 등차가 -2
+            return String.format("%s%s\n", getRepeatedSymbol(" ", pivot - i), getRepeatedSymbol("*", 2 * i + 1));
+        } else {
+            // 역피라미드 출력
+            return String.format("%s%s\n", getRepeatedSymbol(" ", i - pivot), getRepeatedSymbol("*", 2 * (h - i) - 1));
+        }
+    }
+
     public static void main(String[] args) {
         int h = 7;
         int pivot = h / 2;
         for (int i = 0; i < h; i++) {
 //            System.out.printf("pivot: %d %d %d\n", pivot, i, -2 + h - i - 2);
-            if (i <= pivot) {
-                // 피라미드 출력
-                // 초항이 3 등차가 -2
-                System.out.printf("%s%s\n", getRepeatedSymbol(" ", pivot - i), getRepeatedSymbol("*", 2 * i + 1));
-            } else {
-                // 역피라미드 출력
-                System.out.printf("%s%s\n", getRepeatedSymbol(" ", i - pivot), getRepeatedSymbol("*", 2 * (h - i) - 1));
-            }
+            System.out.printf(makeALine(h, i));
         }
     }
 }
