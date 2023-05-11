f13b1c9c01bb7cc861bbe5cf09a6200b18d90d68
sumin <csumin008255@gmail.com>
2023-05-11 17:03:47 +0900

    feat: User, UserTest 추가

---
+++ course_student/java-project2/src/main/java/com/example/javaproject2/week4/day4/User.java
@@ -0,0 +1,45 @@
+package com.example.javaproject2.week4.day4;
+
+public class User {
+    private String name;
+    private String phoneNumber;
+    private int age;
+
+    public User() {
+
+    }
+
+    public User(String name, String phoneNumber, int age) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.age = age;
+    }
+
+    boolean isAdult() {
+        return age >= 18;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
