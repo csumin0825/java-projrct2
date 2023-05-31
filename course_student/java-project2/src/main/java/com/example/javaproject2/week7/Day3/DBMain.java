d9f3e4b69f210f77811d263c03d4770db290e13b
sumin <csumin008255@gmail.com>
2023-05-31 14:20:35 +0900

    feat: DBMain, Person - Person 전체 조회

--- course_student/java-project2/src/main/java/com/example/javaproject2/week7/Day3/DBMain.java
+++ course_student/java-project2/src/main/java/com/example/javaproject2/week7/Day3/DBMain.java
@@ -1,46 +1,51 @@
 package com.example.javaproject2.week7.Day3;
 
 import com.example.javaproject2.week7.Day3.model.BaseDAO;
+import com.example.javaproject2.week7.Day3.model.Person;
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class DBMain extends BaseDAO {
     public static void main(String[] args) {
         DBMain dbMain = new DBMain();
         dbMain.initPerson();
+        dbMain.insertPerson("leo");
+        dbMain.insertPerson("yui");
+        List<Person> personList = dbMain.findAllPerson();
+        System.out.println(personList.toString());
     }
 
-    private void initPerson() {
-        try {
-            getConn();
-            smt = conn.createStatement();
-            smt.setQueryTimeout(30);  // set timeout to 30 sec.
-
-            smt.executeUpdate("drop table if exists person");
-            smt.executeUpdate("create table person (id integer, name string)");
-            smt.executeUpdate("insert into person values(1, 'leo')");
-            smt.executeUpdate("insert into person values(2, 'yui')");
-            rs = smt.executeQuery("select * from person");
-            while (rs.next()) {
-                System.out.println("name = " + rs.getString("name"));
-                System.out.println("id = " + rs.getInt("id"));
-            }
-        } catch (SQLException e) {
-            System.err.println(e.getMessage());
-        } finally {
-            close();
-        }
-    }
-
-    public void getCodeName() throws SQLException {
-        String sql = "SELECT code, name from country order by code, name";
+    private List<Person> findAllPerson() {
+        List<Person> result = new ArrayList<>();
+        String sql = "select id, name from person";
         try {
             getConn();
             psmt = conn.prepareStatement(sql);
             rs = psmt.executeQuery();
             while (rs.next()) {
-                System.out.print(rs.getString("code") + "\t");
-                System.out.println(rs.getString("name"));
+                int id =rs.getInt("id");
+                String name=rs.getString("name");
+                result.add(new Person(id,name));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            close();
+        }
+        return result;
+    }
+
+    public void getCodeName() {
+        String sql = "SELECT id, name from person";
+        try {
+            getConn();
+            psmt = conn.prepareStatement(sql);
+            rs = psmt.executeQuery();
+            while (rs.next()) {
+                int id =rs.getInt("code");
+                String name=rs.getString("name");
             }
         } catch (SQLException e) {
             e.printStackTrace();
@@ -49,4 +54,40 @@ public class DBMain extends BaseDAO {
         }
 
     }
+
+    private int insertPerson(String name){
+        int cnt = 0;
+        String sql = "insert into person(name) vlaue(?)";
+        try{
+            getConn();
+            psmt = conn.prepareStatement(sql);
+            psmt.setString(1, name);
+            psmt.executeUpdate();
+        }catch (SQLException e){
+            System.out.println(e.getMessage());
+        }finally {
+            close();
+        }
+        return cnt;
+    }
+
+    private void initPerson() {
+        String sql= """
+                    create table person(
+                    id integer primary key autoincrement,
+                    name string
+                    """;
+        try {
+            getConn();
+            smt = conn.createStatement();
+
+            smt.executeUpdate("drop table if exists person");
+            smt.executeUpdate(sql);
+        } catch (SQLException e) {
+            System.err.println(e.getMessage());
+        } finally {
+            close();
+        }
+    }
+
 }
