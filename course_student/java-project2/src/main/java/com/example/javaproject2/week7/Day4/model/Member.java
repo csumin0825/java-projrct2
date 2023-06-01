eaf57e50f868e8c17d31086bddfccefa22c48c0f
sumin <csumin008255@gmail.com>
2023-06-01 11:48:41 +0900

    feat: DAOTest, MemberDAO, Member - Member DAO, DTO 구현

--- course_student/java-project2/src/main/java/com/example/javaproject2/week7/Day4/model/Member.java
+++ course_student/java-project2/src/main/java/com/example/javaproject2/week7/Day4/model/Member.java
@@ -1,11 +1,19 @@
 package com.example.javaproject2.week7.Day4.model;
 
 public class Member {
+    String id;
+    boolean isLogined;
+
+    public Member(String id, boolean isLogined) {
+        this.id = id;
+        this.isLogined = isLogined;
+    }
+
     public String getID() {
-        return null;
+        return id;
     }
 
     public boolean isLogined() {
-        return false;
+        return isLogined;
     }
 }
--- course_student/java-project2/src/main/java/com/example/javaproject2/week7/Day4/test/DAOTest.java
+++ course_student/java-project2/src/main/java/com/example/javaproject2/week7/Day4/test/DAOTest.java
@@ -12,14 +12,14 @@ import static junit.framework.TestCase.assertTrue;
 
 public class DAOTest {
     @Test
-    public void testLoginAndLogout(){
+    public void testLoginAndLogout() {
         String id = "tester";
         String pw = "1234";
 
         MemberDAO dao = new MemberDAO();
         dao.initMember();
-        Optional<Member> user = dao.login(id,pw);
-        if(user.isEmpty()){
+        Optional<Member> user = dao.login(id, pw);
+        if (user.isEmpty()) {
             dao.join(id, pw);
             user = dao.login(id, pw);
         }
