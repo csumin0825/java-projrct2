21d7903b1ebf10f014ada05b8bfbcac8a2762ef3
sumin <csumin008255@gmail.com>
2023-06-01 10:07:50 +0900

    feat: OpGame, PlusOperatorLevelOne - 게임 규칙을 인터페이스로 분리완료

--- course_student/java-project2/src/main/java/com/example/javaproject2/week7/Day4/service/OpGame.java
+++ course_student/java-project2/src/main/java/com/example/javaproject2/week7/Day4/service/OpGame.java
@@ -17,13 +17,10 @@ public class OpGame {
     }
 
     public void makeQuestion(int max) {
-//        a = (int) (Math.random() * max);
-//        b = (int) (Math.random() * (max - a));
         op.generateQuestion(max);
     }
 
     public String getQuestion() {
-//        return String.format("%d + %d = ", a, b);
         return op.getQuestionMsg();
     }
 
@@ -33,7 +30,6 @@ public class OpGame {
 
     public boolean isAnswer(int answer) {
         remainingAnswers--;
-//        return (a + b) == answer;
         return op.isEquals(answer);
     }
 
--- course_student/java-project2/src/main/java/com/example/javaproject2/week7/Day4/service/PlusOperatorLevelOne.java
+++ course_student/java-project2/src/main/java/com/example/javaproject2/week7/Day4/service/PlusOperatorLevelOne.java
@@ -1,28 +1,32 @@
 package com.example.javaproject2.week7.Day4.service;
 
-public class PlusOperatorLevelOne implements IOperator{
+public class PlusOperatorLevelOne implements IOperator {
+    int a, b;
+
     @Override
     public void setA(int i) {
-
+        this.a = a;
     }
 
     @Override
     public void setB(int i) {
-
+        this.b = b;
     }
 
     @Override
     public void generateQuestion(int max) {
+        a = (int) (Math.random() * max);
+        b = (int) (Math.random() * (max - a));
 
     }
 
     @Override
     public String getQuestionMsg() {
-        return null;
+        return String.format("%d + %d = ", a, b);
     }
 
     @Override
     public boolean isEquals(int answer) {
-        return false;
+        return (a + b) == answer;
     }
 }
